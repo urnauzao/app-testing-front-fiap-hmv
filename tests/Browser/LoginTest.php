428962bf053c4f2c109ae9b89ff717cc609f5c2b
Urnauzão <julio.cesar.08.08.93@gmail.com>
2022-03-27 21:56:10 -0300

    second commit

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+
+    public $appRoute = "http://localhost:3000";
+    public function testeHomePage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit($this->appRoute)->assertSee('Connect HMV');
+        });
+    }
+
+    public function testeLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit($this->appRoute)
+            ->press('Quero me registrar!')
+            ->assertSee('Digite um e-mail:');
+        });
+    }
+}
